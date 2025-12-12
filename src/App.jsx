ffc38f1b0ff6f6d44d496304185ca46ca678f9df
Santino <127140452+Sanpa33@users.noreply.github.com>
2025-12-12 14:37:01 -0300

    agrega funcionalidad de agregar ingredientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./Header.jsx"
+import Main from "./Main.jsx"
 import './App.css'
 
 
 function App() {
 
+
   return (
     <>
       <Header />
+      <Main/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+export default function Main() {
+
+    
+    const [ingredientsList, setIngredientsList] = React.useState([])
+    
+    console.log(ingredientsList)
+    
+    const ingredientsListItems = ingredientsList.map(ingredientList => (
+        <li key={ingredientList}>{ingredientList}</li>
+    ))
+
+
+    function addIngredient (newIngredient){
+        setIngredientsList(prevIngredient => [...prevIngredient,newIngredient]) 
+    }
+
+
+    function handleSubmit(event) {
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        addIngredient(newIngredient)
+    }
+    
+
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main>
+    )
+}
